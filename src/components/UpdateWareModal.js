a0fa788e05bf66143295d2c5bdc62fb90e0f30fb
bratishques <gurda999@gmail.com>
2020-09-25 00:39:33 +0300

    Added UpdateWare Functionality

--- src/components/UpdateWareModal.js
+++ src/components/UpdateWareModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useState} from "react"
 import AddWareImage from "./AddWareImage"
 import AddWareInput from "./AddWareInput"
 import CategoryInput from "./CategoryInput"
@@ -6,6 +6,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 export const UpdateWareModal = props => {
   const { name, image, price, inStock , description, isUpdating, setIsUpdating, category} = props
+  const [loading, setLoading] = useState(false)
 
   const data = useStaticQuery(graphql`
     query($category: String){
@@ -28,7 +29,7 @@ export const UpdateWareModal = props => {
   }
 
   const categoryName = filterCategory(data.allMongodbGatsbyShopCategories.nodes)
-  console.log(categoryName)
+
 
   const defForm = {
     Name: {
@@ -65,7 +66,6 @@ export const UpdateWareModal = props => {
         if (!form[prop].isValid) return false
       }
       if (file.image === false) {
-        return false
       }
       return true
     }
@@ -97,20 +97,19 @@ export const UpdateWareModal = props => {
     }
   }
   const uploadFile = async () => {
+    setLoading(true)
     const data = new FormData()
-    const { Name} = form
+    const {Name} = form
 
     if (file.image) {
     data.append("file", file.image, Name["value"])
-   }
-
+    }
     for (let key in form) {
       data.append(key, form[key]["value"])
     }
-    setFile({
-      image: false,
-      preview: image,
-    })
+    for (let key in defForm) {
+      data.append("def" + key, defForm[key]["value"])
+    }
     console.log(data)
     const response = await fetch(`/api/products/update/`, {
       method: "POST",
@@ -119,11 +118,7 @@ export const UpdateWareModal = props => {
     const respData = await response.json()
     console.log(respData)
     await fetch(`http://localhost:8000/__refresh`, {method: "POST"})
-    setForm(defForm)
-    setFile({
-      image: false,
-      preview: image,
-    })
+    setLoading(false)
   }
 
   if (isUpdating) {
@@ -133,7 +128,7 @@ export const UpdateWareModal = props => {
           <div className="modal-container">
             <div className={`top-modal-container`}>
               <h4 className={`modal-top`}>Choose an Item to Add</h4>
-              <button className={`closeModal`} onClick={closeWindow}>
+              <button className={`closeModal`} onClick={closeWindow} disabled = {loading}>
                 Close
               </button>
             </div>
