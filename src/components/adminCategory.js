eac80ef825a992e480cfa47b6d46229549df4229
bratishques <gurda999@gmail.com>
2020-10-09 00:14:38 +0300

    added updates to category changing page

--- src/components/adminCategory.js
+++ src/components/adminCategory.js
@@ -1,15 +1,45 @@
-import React from "react"
+import React, { useState } from "react"
+import config from "../config"
+import { useHttp } from "../hooks/http.hook"
 
-const adminCategory = (props) => {
-
+const AdminCategory = (props) => {
     const {name} = props
+    const [updating, setIsUpdating] = useState(false)
+    const [updatedValue, setUpdatingValue] = useState(name)
+  
+    const {baseUrl} = config
+    const {request} = useHttp()
+
+    const inputHandler = (e) => {
+        setUpdatingValue(updatedValue)
+    }
+    const clickHandler = () => {
+        setIsUpdating(true)
+    }
+    const updateHandler = async () => {
+        if (updatedValue !== name) {
+            console.log(name,updatedValue)
+            const data = await request(`${baseUrl}api/categories/update`, "POST", {
+                oldcategory: name,
+                newcategory: updatedValue
+            })
+            console.log(data.json())
+        }
+        setIsUpdating(false)
+    }
+
     return (
-        <div>
-            <div>{name}</div>
-            
-            <button>
+        <div style= {{
+            marginBottom: "20px"
+        }}>
+            <input type="text" disabled = {!updating} value={updatedValue} onChange={inputHandler} style={{
+                width: "50%"
+            }}/>
+            {!updating ? <button onClick = {clickHandler}>
                 Update
-            </button>
+            </button> : <button onClick = {updateHandler}>
+                Finish
+            </button>}
             <button>
                 Delete
             </button>
@@ -17,4 +47,4 @@ const adminCategory = (props) => {
     )
 }
 
-export default adminCategory
+export default AdminCategory
